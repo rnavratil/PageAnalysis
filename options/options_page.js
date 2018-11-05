8e73e5c85c9198673d14614e9d41d90a60dc6024
ruf <r.navratil@outlook.com>
2018-11-05 21:16:37 +0100

    Add sets up selected option.

--- options/options_page.js
+++ options/options_page.js
@@ -10,3 +10,27 @@ document.getElementById('confirm-theme').addEventListener('click', function(){
 document.getElementById('confirm-hideElement').addEventListener('click', function(){
     browser.storage.local.set({myHideElement: document.getElementById('hideElement-browser').value})
 })
+/**
+ * Sets up selected option for theme.
+ */
+browser.storage.local.get('myTheme')
+.then(response => {
+  if(response.myTheme === "dark"){
+    document.getElementById("theme-browser").value = "dark";
+  }
+  else if(response.myTheme === "classic"){
+    document.getElementById("theme-browser").value = "classic";
+  };
+})
+/**
+ * Sets up selected option for 'Search hidden elements'.
+ */
+browser.storage.local.get('myHideElement')
+.then(response => {
+  if(response.myHideElement === "yes"){
+    document.getElementById("hideElement-browser").value = "yes";
+  }
+  else if(response.myHideElement === "no"){
+    document.getElementById("hideElement-browser").value = "no";
+  };
+})
