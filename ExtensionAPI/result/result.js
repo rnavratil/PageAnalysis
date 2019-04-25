e7f24492a6e5a660cb4aeae3fa3aa8b2085bee6c
ruf <r.navratil@outlook.com>
2019-04-25 10:28:19 +0200

    Repair title URL

--- ExtensionAPI/result/result.js
+++ ExtensionAPI/result/result.js
@@ -2,13 +2,24 @@
  * Set new html body for result.html.
  */
 chrome.storage.local.get(['htmlFromServer'], function(result) {
+    // Body
     let parser = new DOMParser();
     let HTMLdoc = parser.parseFromString(result.htmlFromServer, "text/html");
     let newBody = HTMLdoc.querySelectorAll('body');
     document.body = newBody[0];
+
+    // Title URL
     document.title = 'PA -';
-    document.title += ' '.concat(HTMLdoc.querySelector('title').innerText);
-    
+    let titleURL = HTMLdoc.querySelector('title').innerText;
+    var patt = /^https?:\/\/www./i;
+    var RegexResult = titleURL.match(patt);
+    if(RegexResult[0] == 'https://www.'){
+        document.title += ' '.concat(titleURL.substring(12));
+    }else if(RegexResult[0] == 'http://www.'){
+        document.title += ' '.concat(titleURL.substring(11));
+    }else{
+        document.title += ' '.concat(titleURL);
+    }
 });
 
 /**
